11c955e8471938b7865b2c6ff64693eb10afe051
alehunt <alehunt@gmail.com>
2023-06-15 15:50:30 -0300

    Sin sitio_admin

--- pig_23320/urls.py
+++ pig_23320/urls.py
@@ -18,12 +18,12 @@ from django.urls.conf import include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from administracion.admin import sitio_admin # Se debe cargar en admin.py
+# from administracion.admin import sitio_admin # Se debe cargar en admin.py
 
 urlpatterns = [
     path('api/', include('recursos_api.urls')),
     path('', include('portal.urls')),
     path('admin/', admin.site.urls),
-    path('sitio_admin/', sitio_admin.urls), # Se debe cargar en admin.py
+    # path('sitio_admin/', sitio_admin.urls), # Se debe cargar en admin.py
     path('administracion/', include('administracion.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
